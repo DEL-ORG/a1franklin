b04efb92eec494189b6fc11006a012e9ac25d05c
devopseasylearning <info@devopseasylearning.com>
2024-02-23 21:11:24 -0700

    jenkins-server

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "gymef_vpc" {
   cidr_block           = var.vpc_cidr_block # Set your desired CIDR block for the VPC
   enable_dns_support   = var.enable_dns_support
-  enable_dns_hostnames = var.enable_dns_hostnames
+  c
 
   tags = merge(var.common_tags, {
     Name = " gymef_vpc"
