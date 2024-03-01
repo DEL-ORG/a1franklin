3016a9453b17c71a1bc4a9f017c5861db1a8b9d1
devopseasylearning <info@devopseasylearning.com>
2024-03-01 13:07:12 -0700

    aurora postgress

---
+++ gymef2-aurora-postgres/main.tf
@@ -0,0 +1,56 @@
+resource "aws_db_subnet_group" "gymef_subnet_group" {
+  name = "gymef-subnet-group"
+  subnet_ids = var.subnet_ids
+}
+
+resource "aws_rds_cluster_parameter_group" "gymef_cluster_parameter_group" {
+  name        = "gymef-cluster-parameter-group"
+  family      = "aurora-postgresql11"
+  description = "Cluster parameter group"
+}
+
+resource "aws_rds_cluster" "aurora-cluster" {
+  cluster_identifier              = "gymef-aurora-postgres-cluster"
+  engine                          = "aurora-postgresql"
+  engine_version                  = "11.9"
+  database_name                   = "gymef_database"
+  master_username                 = "successgymef"
+  master_password                 = var.master_password
+  db_subnet_group_name            = aws_db_subnet_group.gymef_subnet_group.name
+  vpc_security_group_ids          = [var.security_group_id]
+  db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.gymef_cluster_parameter_group.name
+  skip_final_snapshot             = true # Set to false if you want to take a final snapshot when deleting the cluster
+
+  storage_encrypted       = true
+  backup_retention_period = 9
+  preferred_backup_window = "09:00-11:00"
+  deletion_protection     = false
+  lifecycle {
+    ignore_changes = [
+      engine_version
+    ]
+  }
+  tags = {
+    Name = "gymef-aurora-postgres-cluster"
+  }
+}
+
+resource "aws_rds_cluster_instance" "gymef_cluster_instances" {
+  count = length(var.subnet_ids)
+  cluster_identifier   = aws_rds_cluster.aurora-cluster.id
+  instance_class       = "db.r5.large"
+  engine               = "aurora-postgresql"
+  engine_version       = "11.9"
+  publicly_accessible  = true
+  db_subnet_group_name = aws_db_subnet_group.gymef_subnet_group.name
+  identifier           = "gymef-${count.index + 1}"
+  lifecycle {
+    ignore_changes = [
+      engine_version
+    ]
+  }
+  tags = merge(var.common_tags, {
+    Name = format("%s-gymef_cluster_instances-${count.index}", var.common_tags["project"])
+    },
+  )
+}
---
+++ gymef2-aurora-postgres/variable.tf
@@ -0,0 +1,37 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs for the RDS cluster"
+  type        = list(string)
+  default     = ["subnet-0ec8f617a33e20ec2", "subnet-0e4a88f795e88fd67"]
+
+}
+
+variable "security_group_id" {
+  description = "Security group ID for the RDS cluster"
+  type        = string
+  default     = "sg-03c77da029413afcd"
+}
+
+variable "master_password" {
+  description = "Master password for the RDS cluster"
+  type        = string
+  default     = "Pssw0rd123!"
+}
+
+variable "common_tags" {
+  type = map(any)
+  default = {
+    "id"             = "9999"
+    "owner"          = "gymef"
+    "teams"          = "gym"
+    "environment"    = "dev"
+    "project"        = "aurora-postgres"
+    "create_by"      = "gym-Terraform"
+    "cloud_provider" = "aws"
+  }
+}
+
